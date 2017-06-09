3d20f96272b39cb08ba88233f3e3f8e1826ce6ee
Miguel Sanchez <mangelsr@live.com>
2017-06-08 19:25:10 -0500

    Se corrigio asignacion de funcion encontrarOcurrencia y se verifica la respuesta antes de mostrarla,  se presentan valores de prueba para la funcion intercambiar antes y despues de aplicar la funcion

--- taller.c
+++ taller.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void intercambiar(int *a, int *b);
 int* encontrarOcurrencia(int *x ,int v, int len);
@@ -7,19 +7,21 @@ void desglosar(char *f);
 int main(int argc,char** argv){
 
 	printf("Integrantes:\n- Miguel Sanchez\n- Luis Lama\n\n");
-	
+
 	desglosar("2017-06-25");
-	
+
 	int n[5] = {1,2,3,4,5};
-	
-	printf("\nDireccion del elemento: %p\nEl valor: %d\n",encontrarOcurrencia(n,3,5),*encontrarOcurrencia(n,3,5));
-	
+	int * respuesta = encontrarOcurrencia(n,2,5);
+	if (respuesta != NULL)
+		printf("\nDireccion del elemento: %p\nEl valor: %d\n",respuesta,*respuesta);
+
 	int a = 4;
 	int b = 7;
 
+	printf("\nValores iniciales\na: %d, b: %d\n",a,b);
 	intercambiar(&a,&b);
-	printf("a: %d, b: %d\n",a,b);
-	
+	printf("\nValores al intercambiar\na: %d, b: %d\n",a,b);
+
 	return 0;
 }
 
@@ -32,18 +34,14 @@ void desglosar(char *f){
 int* encontrarOcurrencia(int *x ,int v, int len){
 	for(int i = 0; i < len; i++){
 		if (*(x+i)==v){
-			//printf("Direccion del elemento: %p\n",(x+i));
 			return (x+i);
 		}
 	}
 	return NULL;
 }
 
-
-
 void intercambiar(int *a, int *b){
 	int z = *a;
 	*a = *b;
 	*b = z;
 }
-
